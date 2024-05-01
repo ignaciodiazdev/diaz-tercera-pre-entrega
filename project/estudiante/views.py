19aa7dcb693a6c2cf51e84819271e7defcb19c71
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-01 00:28:22 -0500

    add: views de Create y List para app_estudiante

--- project/estudiante/views.py
+++ project/estudiante/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from . import models, forms
 # Create your views here.
+
+
+def home(request):
+    return render(request, "estudiante/index.html")
+
+
+def estudiante_list(request):
+    consulta = request.GET.get("consulta", None)
+    if consulta:
+        print(consulta)
+        query = models.Estudiante.objects.filter(
+            Q(nombre__icontains=consulta) | Q(apellido__icontains=consulta))
+    else:
+        query = models.Estudiante.objects.all()
+
+    context = {"estudiantes": query}
+    return render(request, "estudiante/estudiante_list.html", context)
+
+
+def estudiante_create(request):
+    if request.method == "POST":
+        form = forms.EstudianteForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect("estudiante:home")
+    else:
+        form = forms.EstudianteForm()
+    return render(request, "estudiante/estudiante_create.html", context={"form": form})
