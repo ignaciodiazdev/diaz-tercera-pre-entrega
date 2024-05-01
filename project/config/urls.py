61596ca3d5c6e3cf0e8ed930db0588b0be288367
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-01 12:51:36 -0500

    include: app_curso en config/urls.py

--- project/config/urls.py
+++ project/config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('estudiante/', include('estudiante.urls')),
+    path('curso/', include('curso.urls')),
 ]
