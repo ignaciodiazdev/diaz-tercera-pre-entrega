229fc13a63b117c940cd2a269c6c0951505da084
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-20 12:45:03 -0500

    fix(estudiante): modificando django administration

--- project/estudiante/admin.py
+++ project/estudiante/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Estudiante)
+
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "apellido",
+        "email",
+        "telefono",
+        "fecha_registro",
+    )
+    list_display_links = ("nombre", "apellido")
+    search_fields = ("nombre", "apellido")
+    ordering = ("nombre", "apellido")
+    date_hierarchy = "fecha_registro"
+
+
+admin.site.register(models.Estudiante, EstudianteAdmin)
